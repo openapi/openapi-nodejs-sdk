960b6fb2e56701afd4fe59ef55b19f8dee3f03ce
Lorenzo Paderi <l.paderi@altravia.com>
2021-06-23 18:48:05 +0200

    fix: search domain as substring of baseurl

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import { ScopeObject } from "..";
 
 export function isServiceInScopes(scopes: Array<ScopeObject>, baseUrl: string) {
     return scopes.find(scope => {
-        return scope.domain === baseUrl;
+        return scope.domain.match(baseUrl);
     });
 }
 
