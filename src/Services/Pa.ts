3e5c37a3820dc2004f660e2ca58ef650120c5939
Lorenzo Paderi <l.paderi@altravia.com>
2021-06-25 13:00:06 +0200

    feat: add pa

---
+++ src/Services/Pa.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosInstance } from "axios";
+import { Environment, Service, } from "..";
+import { getBaseUrl } from "../utils";
+
+export class Geocoding implements Service {
+    client: AxiosInstance;
+    readonly service = 'pa';
+    readonly baseUrl = 'pa.openapi.it';
+    environment: Environment;
+
+    constructor(client: AxiosInstance, environment: Environment) {
+        this.client = client;
+        this.environment = environment;
+    }
+
+    async findPa(partitaIva?: string) {
+        return await (await this.client.get(this.url + '/' + partitaIva)).data.data;
+    }
+
+    get url() {
+        return getBaseUrl(this.environment, this.baseUrl)
+    }
+}
